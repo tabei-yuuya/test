308a88687276b18657417fa814b05e373b61c7af
yuyatabei <ajt.ajt.t@icloud.com>
2023-06-26 11:06:51 +0900

    最初のコミット

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(function () {
+  $(window).on('scroll', function () {
+      if ($('.fv').height() < $(this).scrollTop()) {
+          $('.js-header').addClass('change-color');
+    } else {
+          $('.js-header').removeClass('change-color');
+    }
+  });
+});
+
+$(function () {
+  // ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer, .js-drawer a").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
+});
+
+$(function () {
+  $(".js-accordion__item:first-child .js-accordion__content").css(
+    "display",
+    "block"
+  );
+  $(".js-accordion__item:first-child .js-accordion__title").addClass("is-open");
+  $(".js-accordion__title").on("click", function () {
+    $(this).toggleClass("is-open");
+    $(this).next().slideToggle(300);
+  });
+});
+
+// スワイパー
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// タブ切り替え
+$(function () {
+  const tabButton = $(".js-tab-button"),
+    tabContent = $(".js-tab-content");
+  tabButton.on("click", function () {
+    let index = tabButton.index(this);
+
+    tabButton.removeClass("is-active");
+    $(this).addClass("is-active");
+    tabContent.removeClass("is-active");
+    tabContent.eq(index).addClass("is-active");
+  });
+});
+
